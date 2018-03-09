f96c95a60c66073531b0dec0a68121b7305982e1
Juan Mier <underscoreBis@gmail.com>
2018-03-10 00:59:23 +0100

    1.2

--- gpasser.cmd
+++ gpasser.cmd
@@ -1,5 +1,5 @@
 @echo off
-pushd & color & title gpasser
+pushd & color & title gpasser[%processor_level%]
 
 
 if NOT [%1]==[] set "target=%1"
@@ -11,9 +11,12 @@ goto mashMD5
 
 :mashMD5
 echo Working...
+certutil -hashfile -v %target%
 CertUtil -hashfile %target% MD5
+CertUtil -hashfile %target% SHA256
 echo %random%-%processor_revision%-%processor_level%-%random%>>%target%
 CertUtil -hashfile %target% MD5
+CertUtil -hashfile %target% SHA256
 Ren %target% %processor_revision%%RANDOM%%errorlevel%%RANDOM%%highestnumanodenumber%%RANDOM%%processor_level%%RANDOM%.exe"
 echo Done. & echo.
 pause & exit
