5298960f99726f2ef49dea10d04f338ff8e01cbd
Juan Mier <underscoreBis@gmail.com>
2018-03-10 00:30:54 +0100

    1.1

--- gpasser.cmd
+++ gpasser.cmd
@@ -1,10 +1,19 @@
 @echo off
 pushd & color & title gpasser
-set /p "target=Select exe target:"
+
+
+if NOT [%1]==[] set "target=%1"
+
+
+if NOT DEFINED target set /p "target=Select exe target:" & goto mashMD5
+echo Select exe target:%target%
+goto mashMD5
+
+:mashMD5
 echo Working...
 CertUtil -hashfile %target% MD5
 echo %random%-%processor_revision%-%processor_level%-%random%>>%target%
 CertUtil -hashfile %target% MD5
 Ren %target% %processor_revision%%RANDOM%%errorlevel%%RANDOM%%highestnumanodenumber%%RANDOM%%processor_level%%RANDOM%.exe"
-echo Done.
-pause
+echo Done. & echo.
+pause & exit
