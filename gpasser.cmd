f7d24daad494f4295ee373c82b5ba15abbcb7dde
Juan Mier <underscoreBis@gmail.com>
2018-12-22 15:47:32 +0100

    1.6

--- gpasser.cmd
+++ gpasser.cmd
@@ -1,19 +1,15 @@
 @echo off
-setlocal EnableDelayedExpansion
 pushd & color & title gpasser
+setlocal EnableDelayedExpansion
+
+set alphanum=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
+set string=%alphanum%987654321
+
+if [%1]==[] exit
+set "target=%1"
 
 
-if NOT [%1]==[] set "target=%1"
-
-
-if NOT DEFINED target goto detectInput
-echo Select target root:%target%
-goto caller
-
-:detectInput
-set /p "target=Select target root:"
-
-:caller
+echo Selected target root: %target%
 call :fileExtension %target%
 goto mashMD5
 
@@ -26,31 +22,27 @@ exit /b
 echo Working...
 
 echo %processor_revision%-%random%-%processor_level%-%random%>>%target%
-::changes the raw md5 calculation
-
 CertUtil -hashfile %target% MD2
 CertUtil -hashfile %target% MD4
 CertUtil -hashfile %target% SHA384
 CertUtil -hashfile %target% SHA512
 CertUtil -hashfile %target% MD5
 CertUtil -hashfile %target% SHA256
-::reload hashes
 
 
-Set alphanum=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
-Set string=%alphanum%987654321
+
+
 :_LenLoop
 IF NOT "%string:~18%"=="" SET string=%string:~9%& SET /A _Len+=9& GOTO :_LenLoop
 SET _tmp=%string:~9,1%
 SET /A _Len=_Len+_tmp
-Set count=0
+SET count=0
 SET rndalphanum=
 :_loop
-Set /a count+=1
+SET /a count+=1
 SET _RND=%Random%
-Set /A _RND=_RND%%%_Len%
+SET /A _RND=_RND%%%_Len%
 SET rndalphanum=!rndalphanum!!alphanum:~%_RND%,1!
 If !count! lss 8 goto _loop
 Ren %target% %rndalphanum%%targetex%
-echo Done. & echo.
-pause & exit
+echo Done
